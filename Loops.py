3f31c0ee0e7866027d17b4a9b5edab51c0bf8b7c
fneary <fergus.neary@aig.com>
2019-10-29 16:38:16 +0000

    Solution

---
+++ Loops.py
@@ -0,0 +1,6 @@
+a = """all the apples are green"""
+b= """RHINOS ARE FISH"""
+
+print(a.capitalize())
+print(b.casefold())
+print (a.endswith("green"))
---
+++ Python Division.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+#This contains integer division a//b i.e. To a whole number
+print(a//b) 
+
+#This contains float division i.e. decimal places
+print(a/b)
